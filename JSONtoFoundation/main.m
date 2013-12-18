9fd8d6913c363841b8fa2ea67a6c06113b922dba
Frank Michael Sanchez <msanchez1701@me.com>
2013-12-18 14:11:37 -0500

    Initial Commit

---
+++ JSONtoFoundation/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  JSONtoFoundation
+//
+//  Created by Frank Michael on 12/17/13.
+//  Copyright (c) 2013 Frank Michael. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
