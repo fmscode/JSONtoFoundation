2800dc80e7f536a4b13a82635a7157843f7528be
Frank Michael Sanchez <orion1701@me.com>
2014-08-24 09:29:22 -0400

    New Project and changed name to JSON Convert

---
+++ JSON Convert/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JSON Convert
+//
+//  Created by Frank Michael on 8/24/14.
+//  Copyright (c) 2014 Frank Michael Sanchez. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ JSON Convert/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  JSON Convert
+//
+//  Created by Frank Michael on 8/24/14.
+//  Copyright (c) 2014 Frank Michael Sanchez. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
